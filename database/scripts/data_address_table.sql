4156d20a600227b6cc4b4bcdbb2e928b23854948
Jacob Picado <njacobpg02@gmail.com>
2023-01-02 19:45:50 -0600

    Scripts with data of tables address and province

--- database/scripts/data_address_table.sql
+++ database/scripts/data_address_table.sql
@@ -1,22 +1,99 @@
-use db-officia;
---San José's cantons
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'San José',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Escazú',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Desamparados',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Puriscal',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Tarrazú',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Aserrí',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Mora',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Goicoechea',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Santa Ana',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Alajuelita',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Vázquez de Coronado',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Acosta',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Tibás',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Moravia',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Montes de Oca',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Turrubares',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Dota',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Curridabat',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Pérez Zeledón',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
-INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'León Cortés Castro',1,'2023-01-02 14:03:47','2023-01-02 14:03:47');
+USE `db-officia`;
+
+#San José
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'San José',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Escazú',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Desamparados',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Puriscal',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Tarrazú',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Aserrí',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Mora',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Goicoechea',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Santa Ana',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Alajuelita',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Vázquez de Coronado',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Acosta',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Tibás',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Moravia',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Montes de Oca',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Turrubares',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Dota',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Curridabat',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Pérez Zeledón',1,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'León Cortés Castro',1,NOW(),NOW());
+
+#Alajuela
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Alajuela',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'San Ramón',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Grecia',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'San Mateo',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Atenas',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Naranjo',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Palmares',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Poás',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Orotina',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'San Carlos',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Zarcero',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Sarchí',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Upala',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Los Chiles',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Guatuso',2,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Río Cuarto',2,NOW(),NOW());
+
+#Cartago
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Cartago',3,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Paraíso',3,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'La Unión',3,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Jiménez',3,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Turrialba',3,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Alvarado',3,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Oreamuno',3,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'El Guarco',3,NOW(),NOW());
+
+#Heredia
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Heredia',4,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Barva',4,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Santo Domingo',4,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Santa Bárbara',4,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'San Rafael',4,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'San Isidro',4,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Belén',4,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Flores',4,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'San Pablo',4,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Sarapiquí',4,NOW(),NOW());
+
+#Guanacaste
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Liberia',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Nicoya',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Santa Cruz',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Bagaces',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Carrillo',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Cañas',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Abangares',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Tilarán',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Nandayure',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'La Cruz',5,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Hojancha',5,NOW(),NOW());
+
+#Puntarenas
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Puntarenas',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Esparza',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Buenos Aires',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Montes de Oro',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Osa',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Quepos',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Golfito',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Coto Brus',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Parrita',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Corredores',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Garabito',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Monteverde',6,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Puerto Jiménez',6,NOW(),NOW());
+
+#Limón
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Limón',7,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Pococí',7,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Siquirres',7,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Talamanca',7,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Matina',7,NOW(),NOW());
+INSERT INTO `address`(`name`, `province_id`, `created_at`, `updated_at` )VALUES( 'Guácimo',7,NOW(),NOW());
---
+++ database/scripts/data_province_table.sql
@@ -0,0 +1,9 @@
+USE `db-officia`;
+
+INSERT INTO `province`(`name`, `created_at`, `updated_at`) VALUES ('San José',NOW(),NOW());
+INSERT INTO `province`(`name`, `created_at`, `updated_at`) VALUES ('Alajuela',NOW(),NOW());
+INSERT INTO `province`(`name`, `created_at`, `updated_at`) VALUES ('Cartago',NOW(),NOW());
+INSERT INTO `province`(`name`, `created_at`, `updated_at`) VALUES ('Heredia',NOW(),NOW());
+INSERT INTO `province`(`name`, `created_at`, `updated_at`) VALUES ('Guanacaste',NOW(),NOW());
+INSERT INTO `province`(`name`, `created_at`, `updated_at`) VALUES ('Puntarenas',NOW(),NOW());
+INSERT INTO `province`(`name`, `created_at`, `updated_at`) VALUES ('Limón',NOW(),NOW());
